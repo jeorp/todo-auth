73da8a6ae2966f4ecc4a3a2f4ff28105ac0a54ee
jeorp <jeorpqxx@gmail.com>
2022-01-17 17:38:14 +0900

    update

--- src/Model/Auth/Flow.hs
+++ src/Model/Auth/Flow.hs
@@ -27,3 +27,4 @@ type TokenInfo = Record '[ "access_token" >: String ]
 
 
 
+
--- src/Utility/RenderCsv.hs
+++ src/Utility/RenderCsv.hs
@@ -2,15 +2,25 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeApplications  #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE PolyKinds #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE CPP                   #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
 
 module Utility.RenderCsv where
 
 import Data.Csv (ToField, FromField, ToNamedRecord, FromNamedRecord, 
-                  toNamedRecord, parseNamedRecord, (.:), (.=))
+                  toNamedRecord, parseNamedRecord, (.:), (.=), NamedRecord)
+import Control.Applicative
+import Data.Constraint
 import Data.Extensible
 import Data.Proxy
 import GHC.TypeLits
+import Data.Monoid
+import Data.Functor.Const
+import Control.Monad.Identity
+import qualified Data.HashMap.Strict as HM
 import Data.String
 import qualified Data.ByteString as B
 
@@ -18,5 +28,15 @@ instance Forall (KeyTargetAre KnownSymbol FromField) xs => FromNamedRecord (Reco
   parseNamedRecord r = hgenerateFor (Proxy @ (KeyTargetAre KnownSymbol FromField)) $ 
     \m -> let k = symbolVal (proxyKeyOf m) in Field . pure <$> r .: fromString k
 
+{- can not compile (; ;)
 instance Forall (KeyTargetAre KnownSymbol ToField) xs => ToNamedRecord (Record xs) where
-  toNamedRecord r = undefined
+  toNamedRecord _ = HM.fromList . flip appEndo mempty . getConst . hgenerateFor (Proxy @ (KeyTargetAre KnownSymbol FromField)) $
+    \m -> let k = symbolVal (proxyKeyOf m) in Field . pure <$> Const $ Endo ((fromString k .= proxyTargetOf m):)
+-}
+
+
+instance Forall (KeyTargetAre KnownSymbol ToField) xs => ToNamedRecord  (Record xs) where
+  toNamedRecord = HM.fromList . flip appEndo [] . hfoldMap getConst . hzipWith
+    (\(Comp Dict) -> Const . Endo . (:) .
+      liftA2 (.=) (fromString . symbolVal . proxyKeyOf) (runIdentity . getField))
+    (library :: xs :& Comp Dict (KeyTargetAre KnownSymbol ToField))
